d9bba6322b6462a473dfce64c5db8c33170db4fb
Youngju-Jang <joj1043@github.com>
2022-09-20 19:27:39 +0900

    edit sth

--- src/main/java/com/jojoldu/book/springboot/web/HelloController.java
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -1,7 +1,7 @@
 package com.jojoldu.book.springboot.web;
 
 import com.jojoldu.book.springboot.web.dto.HelloResponseDto;
-import org.springframework.security.web.authentication.logout.HeaderWriterLogoutHandler;
+//import org.springframework.security.web.authentication.logout.HeaderWriterLogoutHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
